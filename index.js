a16691ad46215ce8f9d0308374d5192d56742401
hotoo <hotoo.cn@gmail.com>
2014-12-10 11:18:04 +0800

    Update set value()

--- index.js
+++ index.js
@@ -53,35 +53,27 @@ var DEFAULT_OPTIONS = {
   defaultOption: null
 };
 
+var DATA_VALUE_ATTR = "data-value";
+
 function Linkage(element, options){
   var me = this;
   me.element = $(element);
   var options = me.options = $.extend({}, DEFAULT_OPTIONS, options);
   me._evt = new Events(me);
-  me._setStatus("init");
 
   if (options.driver) {
     options.driver.on("change", function(key){
       me.render(key);
-    }).on("loading", function(){
-      me._setStatus("loading");
     });
   }
 
   me.element.on("change", function(){
     me._evt.emit("change", this.value);
-  })
-};
-
-Linkage.prototype._setStatus = function(status){
-  var me = this;
-  me.status = status;
-  me._evt.emit(status);
+  });
 };
 
 Linkage.prototype.render = function(key){
   var me = this;
-  me._setStatus("loading");
   var data_option = this.options.data;
   var data = isFunction(data_option) ? data_option(key) : data_option;
 
@@ -96,6 +88,8 @@ Linkage.prototype.render = function(key){
   }
 
   function render(data){
+    var option;
+    var value;
     var select_options = me.element[0].options;
     // Clear original options.
     for(var i = select_options.length - 1; i >= 0; i--){
@@ -104,20 +98,24 @@ Linkage.prototype.render = function(key){
     // Set new options.
     var defaultOption = me.options.defaultOption;
     if (defaultOption) {
-      select_options[0] = createOption(defaultOption, true);
+      option = select_options[0] = createOption(defaultOption, true);
       if (defaultOption.disabled !== false) {
-        select_options[0].disabled = true;
+        option.disabled = true;
       }
+      value = option.value;
     }
     for (var i=0, l=data.length; i<l; i++) {
-      select_options[defaultOption ? i+1 : i] = createOption(data[i]);
+      option = select_options[defaultOption ? i+1 : i] = createOption(data[i]);
+      // XXX: 考虑合并到 createOption(data, value===data_value)
+      if (option.value === me.element.attr(DATA_VALUE_ATTR)) {
+        option.selected = true;
+        option.defaultSelected = true;
+        value = option.value;
+      }
     }
 
     if (select_options.length) {
-      me._evt.emit("change", select_options[0].value);
-    }
-    if (data.length) {
-      me._setStatus("ready");
+      me._evt.emit("change", value);
     }
   }
 
@@ -125,23 +123,25 @@ Linkage.prototype.render = function(key){
 };
 
 Linkage.prototype.val = function(){
+  var me = this;
+
   if (arguments.length === 0) {
-    return this.element.val();
+    return me.element.val();
   }
 
-  var me = this;
   var value = arguments[0];
 
-  if (!me.options.driver || me.status === "ready") {
-    setValue();
-  } else {
-    this.on("ready", setValue);
-  }
+  me.element.attr(DATA_VALUE_ATTR, value);
+  setValue(me.element, value);
 
-  function setValue() {
-    me._evt.off("ready", setValue);
-    me.element.val(value);
-    me._evt.emit("change", value);
+  function setValue(select, value) {
+    select.find(">option").each(function(index, option){
+      if (option.value === value) {
+        option.selected = true;
+        option.defaultSelected = true;
+        me._evt.emit("change", value);
+      }
+    });
   }
 };
 
